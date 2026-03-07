7872c0d51f360f53670ee856a53a1fbfb96675aa
robinghimire <rghimire@icstars.org>
2026-03-06 20:26:25 -0600

    Index.html ready, kcps urls.py updated

--- KCPS/urls.py
+++ KCPS/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='home'), 
     path('admin/', admin.site.urls),
     path('tickets/', include('parts.tickets.urls')),
     path('account/', include('parts.account.urls')),
