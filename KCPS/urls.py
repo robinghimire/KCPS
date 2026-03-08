d31a2af9617242f2e38d37bc1848eb047247da34
robinghimire <rghimire@icstars.org>
2026-03-08 01:52:21 -0600

    ticketing system ready to go.

--- KCPS/urls.py
+++ KCPS/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     path('tickets/', include('parts.tickets.urls')),
     path('account/', include('parts.account.urls')),
     path('content/', include('parts.content.urls')),
+    path('tickets/', include('parts.tickets.urls')),
+
 ]
 
 if settings.DEBUG:
