30ef1b998f93b7fa9aa6803f914b5bed7325fd31
robinghimire <rghimire@icstars.org>
2026-03-09 23:10:06 -0500

    django-tinymce - navbar adjusted

--- KCPS/urls.py
+++ KCPS/urls.py
@@ -19,8 +19,9 @@ from django.urls import path
 from django.urls import include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
-
 from KCPS import settings
+
+
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='home'), 
